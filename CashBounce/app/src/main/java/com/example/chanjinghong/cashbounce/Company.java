7317cb019609e2d6dd34dad93bcf81ad702dfa2f
cjinghong <kismeted97@gmail.com>
2015-06-14 11:30:18 +0800

    Help

--- CashBounce/app/src/main/java/com/example/chanjinghong/cashbounce/Company.java
+++ CashBounce/app/src/main/java/com/example/chanjinghong/cashbounce/Company.java
@@ -1,38 +1,34 @@
 package com.example.chanjinghong.cashbounce;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Company extends ActionBarActivity {
+
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company);
+
+        backButton = (Button) findViewById(R.id.backBtnC);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goHome();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_company, menu);
-        return true;
+    private void goHome() {
+        Intent intent = new Intent(this, Menucehck.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
